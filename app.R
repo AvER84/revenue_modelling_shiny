45d6366db18e3ef9dd45f946789abf5b986e8f2d
Alex Eisenhart <afver84@gmail.com>
2018-12-19 00:26:50 +0000

    Added new statistical analysis, started on the app itself a little

--- app.R
+++ app.R
@@ -6,45 +6,95 @@
 #
 #    http://shiny.rstudio.com/
 #
-
+options(scipen = 999)
 library(shiny)
+library(shinythemes)
+library(lubridate)
+library(dplyr)
+library(ggplot2)
+library(plotly)
+library(DT)
+library(formattable)
+library(broom)
 
-# Define UI for application that draws a histogram
+
 ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a slider input for number of bins 
+
+   titlePanel("Developing a prediction model"),
+
    sidebarLayout(
       sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
+         selectInput(inputId = "predictors", 
+                     label = "Choose predictors",
+                     choices = "No choices yet" )
       ),
-      
-      # Show a plot of the generated distribution
+
       mainPanel(
-         plotOutput("distPlot")
+         plotOutput("past_performance_plot"),
+         formattableOutput("model"),
+         formattableOutput("weekday_model")
       )
    )
 )
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
+  
+  output$base_data <- renderDataTable({
+    historic
+  })
+    
+  output$past_performance_plot <- renderPlot({
+    historic %>%
+      ggplot(aes(x=date, y = revenue)) +
+      geom_line()
+   })
    
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
+   mod <- lm(revenue ~ spend + factor(week_day, ordered=FALSE) + Sale, data = historic)
+   
+   mod_summary <- summary(mod) %>% tidy() %>% 
+     mutate(
+       estimate = round(estimate,3),
+       std.error = round(std.error,3),
+       statistic = round(statistic,3),
+       p.value = round(p.value,3)
+     ) 
+   
+   output$model <- renderFormattable({
+     mod_summary %>% formattable()
+   })
+     
+   output$weekday_model <- renderFormattable({
+     
+     varlist = c("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun")
+     models <- lapply(varlist, function(x) {
+       lm(substitute(revenue ~ spend  + i, list(i = as.name(x))), data = historic) %>% 
+         summary() %>% 
+         broom::tidy() %>% 
+         filter(str_detect(term, 'TRUE')) %>% 
+         mutate_if(is.numeric, round, 3) %>% 
+         mutate(term = substr(term, 1, 3))
+     })
+     
+     weekday_models <- models %>% bind_rows() 
+     
+     models2 <- lapply(varlist, function(x) {
+       lm(substitute(revenue ~ spend + Sale  + i, list(i = as.name(x))), data = historic) %>% 
+         summary() %>% 
+         broom::tidy() %>% 
+         mutate_if(is.numeric, round, 3) %>% 
+         select(term, estimate) %>% 
+         spread(term, estimate) %>% 
+         select(SaleSale, spend) %>% 
+         mutate(term = x)
+     })
+     
+     weekday_models <- weekday_models %>% 
+       left_join(models2 %>% bind_rows(), by = "term")
+     
+     weekday_models %>% formattable()
+     
    })
 }
 
-# Run the application 
 shinyApp(ui = ui, server = server)
 
